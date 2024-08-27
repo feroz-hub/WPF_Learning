89d13f97a3eb8fc4a1285064ede65261913c21fb
Feroze basha S <bashaferoz66@gmail.com>
2024-08-27 20:11:43 +0530

    TextBlock

---
+++ src/TestBlockDemo/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace TestBlockDemo;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow:Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        var psi = new ProcessStartInfo
+        {
+            FileName = e.Uri.AbsoluteUri,
+            UseShellExecute = true
+        };
+        Process.Start(psi);
+        e.Handled = true;
+    }
+}
